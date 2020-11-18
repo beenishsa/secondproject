9f849a78dfdf7b18b5c5f1a02b0440a753435b91
beenishsa <71415197+beenishsa@users.noreply.github.com>
2020-11-17 20:33:01 -0500

    third

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-$(document).ready(function(){
-    console.log("ready!");
+$( document ).ready(function() {
+    console.log( "ready!" );
 
-$('#search-button').on('click', () => {
-    let searchTerm=$('search-field').val();
-    console.log(searchTerm);
-    let url="https://api.giphy.com/v1/gifs/search?api_key=qZqq8jPd45n49RvLiY6PtNPiAixu1RsK=" + searchTerm
-       $.get(url, function(giphyResponse) {
-         console.log(giphyResponse);
-         console.log('giphyData', giphyResponse.data)
-         console.log('giphyData result 0 (1st result)', giphyResponse.data[0]);
-         console.log('giphyData result 0 (1st result) - all images list', giphyResponse.data[0].images);
-         console.log('giphyData result 0 (1st result) - all images list - original image', giphyResponse.data[0].images.original);
+//$('#fetch-from-api').click(function() {
+    //console.log("i was clicked");
+    
+$.get("https://api.giphy.com/v1/gifs/search?api_key=qZqq8jPd45n49RvLiY6PtNPiAixu1RsK&q=corona", function(giphyResponse) {
+    console.log(giphyResponse);
+    console.log('giphyData', giphyResponse.data)
+    console.log('giphyResponse result 0 (1st result)'- giphyResponse.data[0]);
+    console.log('giphyResponse result 0 (1st result) - all images list', giphyResponse.data[0].images);
+    console.log('giphyResponse result 0 (1st result)- all images list- original image', giphyResponse.data[0].images.original);
+    console.log('giphyResponse result 0 (1st result) - all images list- original image-url', giphyResponse.data[0].images.original.url);
+     
+    let imgHtml= "<img src=${giphyResponse.data[0].images.original.url} />"
 
-         console.log('giphyData result 0 (1st result) - all images list - original image - url', giphyResponse.data[0].images.original.url);
+    $('#first-result-image').html(imgHtml);
 
 
-         for (let image of giphyResponse.data) {        
-           let imgHtml = `<img src=${image.images.original.url} />`
-           $('#search-results').append(imgHtml);
-         }
         });
-       })
-});
+    })
+//});
+
 
 
